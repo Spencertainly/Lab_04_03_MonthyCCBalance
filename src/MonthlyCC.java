fa584ec311ee8f14c4d5ef4b770f583b30da48bc
Spencer <salane2232@gmail.com>
2025-09-15 21:30:01 -0400

    Commit 2

--- src/MonthlyCC.java
+++ src/MonthlyCC.java
@@ -1,7 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class MonthlyCC {
     public static void main(String[] args) {
-        
+        System.out.println( "Monthly CC Balance"); //Task 3
+        double balance = 5000.0;
+        double interestRate = 0.17;
+        double firstMonth = 0;
+        double secondMonth = 0;
+
+
+        firstMonth = balance + (balance * interestRate);
+        secondMonth = firstMonth + (firstMonth * interestRate);
+
+
+        System.out.println( " At the end of the first month the balance equals $" + firstMonth);
+        System.out.println( " At the end of the second month the balance equals $" + secondMonth);
+
+
+        System.out.println();
+        System.out.println();
+
     }
 }
